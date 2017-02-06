d24d8d840249016e772b5ea6510b30422d7d4a6d
joe223 <heyin223@163.com>
2017-02-06 20:34:16 +0800

    add:  service

---
+++ src/app/hero.ts
@@ -0,0 +1,4 @@
+export default class Hero {
+    name: string = "[none]";
+    id: string = "[none]";
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import Hero from './hero';
+
+export const HEROES: Hero[] = [
+    {
+        name: "joe",
+        id: "11"
+    },
+    {
+        name: "asdfg",
+        id: "12"
+    },
+    {
+        name: "hsfa",
+        id: "13"
+    },
+    {
+        name: "Swferg",
+        id: "14"
+    },
+];
