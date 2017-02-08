511790f3eed70a60605db2908a1216c8889f5539
joe223 <heyin223@163.com>
2017-02-09 00:18:02 +0800

    add: router component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,3 +4,31 @@ import { RouterModule, Routes } from "@angular/router";
 import HeroDetailsComponent from "./hero-detail.component";
 import HeroesComponent from "./heroes.component";
 import DashboardComponent from "./dashboard.component";
+
+const routes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "detail/:id",
+    component: HeroDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,19 @@ import { RouterModule }   from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 
+import HeroDetailsComponent from "./hero-detail.component";
+import HeroesComponent from "./heroes.component";
+import DashboardComponent from "./dashboard.component";
 
 import HeroService from './hero.service';
 
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: "dashboard",
-        component: DashboardComponent
-      },
-      {
-        path: "detail/:id",
-        component: HeroDetailsComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -39,11 +25,11 @@ import HeroService from './hero.service';
     DashboardComponent,
   ],
   providers: [ HeroService ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 
 
 
 export class AppModule {
-  
+
 }
